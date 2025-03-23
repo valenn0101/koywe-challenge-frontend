66f9de5838a152e19036cd48a8a4aee4a39bc25e
valenn0101 <veroncaceresvalentin@gmail.com>
2025-03-23 22:39:35 +0000

    feat(Layout): add background color

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -12,7 +12,7 @@ export default function Layout({ children }: LayoutProps) {
   return (
     <div className="flex min-h-screen flex-col">
       <Navbar />
-      <main className="flex-1">{children}</main>
+      <main className="flex-1 bg-white">{children}</main>
       <Footer />
     </div>
   );
